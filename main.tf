155554524eb3d1a12a555a09bab4f951ffe49d16
Alex WOO <alex23woo@gmail.com>
2022-09-24 23:23:14 +0800

    aws_launch_configuration

--- main.tf
+++ main.tf
@@ -7,36 +7,16 @@ variable "server_port" {
   type        = number
   default     = 8080
 }
-# Part : security group
-
-resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
-  # Allow ec2 instance to receive traffic on port 8080
-  ingress {
-    from_port = var.server_port
-    to_port = var.server_port
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# Part : Instance
-resource "aws_instance" "example" {
-  ami = "ami-0fb653ca2d3203ac1"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id] # Link the security Group with reference expression
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0fb653ca2d3203ac1"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, World!" > index.html
-              nohup busybox httpd -f -p 8080 &
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
-
-  user_data_replace_on_change = true
-
-  tags = {
-    Name = "terraform-example"
-  }
 }
 
 output "public_ip" {
